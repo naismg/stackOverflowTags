fa2afbd1b68f41711cddae09950cb5f091da8d32
ellana <ellanaca1592@gmail.com>
2023-12-22 13:46:11 +0100

    app

--- app.py
+++ app.py
@@ -10,44 +10,6 @@ import plotly.express as px
 # generate_background_image()
 st.set_page_config(layout="wide")
 
-background_image = 'url("https://www.silicon.fr/wp-content/uploads/2019/08/stack_overflow.jpg")'
-# CSS pour définir l'image de fond
-st.markdown(f"""
-    <style>
-        .stApp {{
-    background-image: {background_image};
-    background-repeat: no-repeat;
-    background-color: white;
-    background-position: cover;
-    height: 50vh; /* Ajustez cette valeur pour changer la hauteur de l'image */
-}}
-
-    .sidebar .sidebar-content {{
-        background: #262730;
-    }}
-    .Widget {{
-        color: white;
-    }}
-    .stTextInput > div > div > input, .stTextArea > div > div > textarea, .stFileUploader > div > input {{
-        background-color: #555e6f;
-        color: white;
-    }}
-    .stTextInput > div > div > input::placeholder, .stTextArea > div > div > textarea::placeholder {{
-        color: white;
-    }}
-    .stCheckbox label, .stRadio label {{
-        color: white;
-    }}
-    .stMarkdown p, .stMarkdown h1, .stMarkdown h2, .stMarkdown h3, .stMarkdown h4, .stMarkdown h5, .stMarkdown h6 {{
-        color: black;
-    }}
-    .stSelectbox div[role="button"] {{
-        color: white;
-    }}
-    </style>
-    """,
-            unsafe_allow_html=True)
-
 
 app = FastAPI()
 
@@ -76,7 +38,6 @@ response2 = requests.get('https://stack-tags.onrender.com/search_runs')
 prediction2 = response2.json()['runs']
 
 df = pd.DataFrame(prediction2)
-
 
 y_options = df.columns.tolist()
 
